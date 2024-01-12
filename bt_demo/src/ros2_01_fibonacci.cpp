4078b1f010e87d2096712262abad7d0f88e6dd3d
Luke Chang <luke@boxfish.nz>
2024-01-12 16:59:54 +1300

    added placeholder for ros2 fibonacci

---
+++ bt_demo/src/ros2_01_fibonacci.cpp
@@ -0,0 +1,17 @@
+#include "action_tutorials_interfaces/action/fibonacci.hpp"
+#include "behaviortree_ros2/bt_action_node.hpp"
+#include "behaviortree_cpp/bt_factory.h"
+
+#include <iostream>
+#include <string>
+
+// class FibonacciAction : public BT::RosActionNode<Fibonacci>
+// {
+
+// };
+
+int main(int argc, char const* argv[])
+{
+    /* code */
+    return 0;
+}
